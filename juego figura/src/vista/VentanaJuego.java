f80d51e1694887baef25c34ac40ad78cdf45a9cf
1960722-2724 <146265513+1960722-2724@users.noreply.github.com>
2023-09-27 16:15:23 -0500

    Add files via upload

---
+++ juego figura/src/vista/VentanaJuego.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+/**
+ *
+ * @author andre
+ */
+public class VentanaJuego {
+   private Header jpheader;
+   
+   private 
+}
