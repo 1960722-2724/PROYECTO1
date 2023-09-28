dd0a9e4add3459db84b6eeb83ea87291d287b43e
1960722-2724 <146265513+1960722-2724@users.noreply.github.com>
2023-09-28 16:07:23 -0500

    Add files via upload

--- juego figura/src/vista/VentanaPrincipal.java
+++ juego figura/src/vista/VentanaPrincipal.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vista;
 
 import modelo.Jugador;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,9 +20,8 @@ import javax.swing.SwingConstants;
  */
 public final class VentanaPrincipal extends JFrame {
     private Header jpHeader;
-    private JLabel jMensaje;
     private JPanel jpContenido;
-    private JLabel jNombre;
+    private JLabel jNombre1;
     private JTextField txtNombre;
     private JButton btnJugar; 
     private JButton btnInstrucciones; 
@@ -39,82 +32,100 @@ public final class VentanaPrincipal extends JFrame {
         iniciarComponentes();
     }
     
-    private void iniciarComponentes(){
-        //recuadro principal
-        setTitle("juego figuras");
+//recuadro principal
+    private void Ventana(){
+    
+        setTitle("juego de figuras");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1000,650);
         setLocationRelativeTo(null);
         setVisible(true); 
         setResizable(false);
         setLayout(null);
+        
+    btnLimpiar = new JButton("Limpiar");
+    btnAceptar = new JButton("Aceptar");
+    btnJugar = new JButton("Iniciar Juego");
+    btnInstrucciones = new JButton("Instrucciones");
     
-    Toolkit miPantalla = Toolkit.getDefaultToolkit();
+    txtNombre = new JTextField(20);
     
-    //configuracion de la vista del juego
-     jpHeader = new Header("/imagenes/header.png"); 
-        jpContenido = new JPanel();
-    
-    jlMensaje = new JLabel("BIENVENIDO",SwingConstants.CENTER);
-        jlNombre = new JLabel("INGRESA TU NOMBRE",SwingConstants.LEFT);
-
+    btnLimpiar.setBounds(50, 550, 150, 35);
+    btnAceptar.setBounds(210, 550, 150, 35);
+    btnJugar.setBounds(600, 500, 150, 35);
+    btnInstrucciones.setBounds(600, 550, 150, 35);
+    txtNombre.setBounds(50, 100, 410, 40);   
     jpHeader.setSize(1000,300);
-    
     jpContenido.setSize(1000,300);
     jpContenido.setBounds(0,110,1000,300);
     jpContenido.setLayout(null);
-
-    add (jpHeader);
-    add (jpContenido);
     
-    jMensaje.setBounds(0,20, 519,20);
-    jMensaje.setForeground(Color.blue);
-    jMensaje.setFont(new Font("arial", Font.BOLD, 20));  
-        
-        
-    jNombre.setBounds(0,130, 519,35);
-    jNombre.setForeground( Color.ORANGE);
-    jNombre.setFont(new Font("arial", Font.BOLD, 20)); 
-    
-    btnJugar = new JButton("Iniciar Juego");
-    btnJugar.setBounds(180,270, 150,35);
-     
-    btnInstrucciones = new JButton ("instrucciones");
-    btnInstrucciones.setBounds(100,190, 150,35);
-     
-    btnLimpiar = new JButton ("limpiar"); 
-    btnLimpiar.setBounds(50,100,150,35);
-    
-    btnAceptar = new JButton ("aceptar");
-    btnAceptar.setBounds(80,130, 150, 30);
-     
-    jpContenido.add(jMensaje);
-    jpContenido.add(jNombre);
+    jpContenido.add(jpHeader);
+    jpContenido.add(jNombre1);
     jpContenido.add(btnJugar);
     jpContenido.add(btnInstrucciones);
     jpContenido.add(btnLimpiar);
     jpContenido.add(btnAceptar);
-    
-    txtNombre = new JTextField("");
-    txtNombre.setHorizontalAlignment(JTextField.LEFT);
-    txtNombre.setForeground(Color.GRAY);
-    txtNombre.setFont(new Font("arial", Font.BOLD, 20));
-    
     jpContenido.add(txtNombre);
+    add (jpHeader);
+    add (jpContenido);
     
-    txtNombre.setBounds(50,190,410, 40);
+    jNombre1 = new JLabel("INGRESA TU NOMBRE");
+    jNombre1.setBounds(50, 50, 519, 35);
     
-    //ORGANIZAR COORDENADAS
+    jpHeader = new Header("/imagenes/header.png"); 
+    jpContenido = new JPanel();
     
-    //MANEJADOR DE EVENTOS
+    ManejadorDeEventos manejadorEventos = new ManejadorDeEventos();
     
-      
+    btnJugar.addActionListener(manejadorEventos);
+    btnInstrucciones.addActionListener(manejadorEventos);
+    btnLimpiar.addActionListener(manejadorEventos);
+    btnAceptar.addActionListener(manejadorEventos);
+    txtNombre.addKeyListener(manejadorEventos);
+    
+    txtNombre.requestFocusInWindow();
+    Toolkit miPantalla = Toolkit.getDefaultToolkit();
+    }
     
     
-    
-    
-    
-    
-    
-    
+//necesito ayuda con la logica de los botones 
+private void ingresoNombre(){
+  String nombre =txtNombre.getText();
+        if(!nombre.trim().isEmpty() || nombre.trim().length() > 0){
+            Jugador jugador = new Jugador(nombre);        
+            VentanaJuego ventanaJuego = new VentanaJuego(jugador);              
+        } else {
+            JOptionPane.showMessageDialog(null,"ingrese un nombre valido", 
+                    "Advertencia", JOptionPane.ERROR_MESSAGE);
+            txtNombre.requestFocusInWindow();
+        }
+    }
+
+private void desBtnJugar(){
+    //funcion que desloquea el boton de jugar cuando ya se haya presionado el boton de aceptar
+    //y esta me lleva al juego como tal
+}
+        
+        
+        
+class ManejadorDeEventos implements ActionListener, KeyListener{
+       
+        public void actionPerformed(ActionEvent evento){
+            if(evento.getSource() == btnIngresar){                
+                iniciarJuego();
+            }
+        }
+        
+        public void keyReleased(KeyEvent e) {
+            if(e.getKeyCode() == e.VK_ENTER){
+                btnJugar.doClick();
+            }
+        }
+        
+        public void keyPressed(KeyEvent e) {   
+        }
+        
+        public void keyTyped(KeyEvent e) {
+        }
     }
