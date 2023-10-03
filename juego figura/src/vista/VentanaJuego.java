9b48aa4d7598db4e3fd6775504c7c2e103088dab
1960722-2724 <146265513+1960722-2724@users.noreply.github.com>
2023-10-03 12:34:56 -0500

    Update VentanaJuego.java

--- juego figura/src/vista/VentanaJuego.java
+++ juego figura/src/vista/VentanaJuego.java
@@ -21,6 +21,10 @@ public class VentanaJuego extends JFrame {
     private JLabel[] figurasLabels;
     private Jugador jugador;
 
+    private int nivel = 1;
+    private int intentos = 0;
+    private int fallos = 0;
+
     public VentanaJuego(Jugador jugador) {
         super("Juego de Figuras");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,16 +41,16 @@ public class VentanaJuego extends JFrame {
 
     private void initComponents() {
         lblNombre = new JLabel("Jugador: " + jugador.getNombre());
-        lblNivel = new JLabel("Nivel: 1");
-        lblIntentos = new JLabel("Intentos: 0");
-        lblFallos = new JLabel("Fallos: 0");
+        lblNivel = new JLabel("Nivel: " + nivel);
+        lblIntentos = new JLabel("Intentos: " + intentos);
+        lblFallos = new JLabel("Fallos: " + fallos);
 
         btnTerminarJuego = new JButton("Terminar Juego");
         figurasLabels = new JLabel[4]; // Mostraremos 4 imágenes en total
 
         List<String> rutas = new ArrayList<>();
-        for (int i = 1; i <= 3; i++) { // Cambiamos a 4 rutas en lugar de 5
-            rutas.add("src/imagenes/nivel1/" + i + ".png");
+        for (int i = 1; i <= 3; i++) {
+            rutas.add("src/imagenes/nivel" + nivel + "/" + i + ".png");
         }
 
         // Shuffle para que las imágenes estén en orden aleatorio
@@ -76,11 +80,6 @@ public class VentanaJuego extends JFrame {
                 lblFiguraMuestra = labelOpcion;
             }
         }
-
-        System.out.println("Ruta de la imagen de muestra: " + lblFiguraMuestra.getName());
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Ruta de opción " + (i + 1) + ": " + figurasLabels[i].getName());
-        }
     }
 
     private void setupLayout() {
@@ -89,13 +88,12 @@ public class VentanaJuego extends JFrame {
         JPanel panelFiguraMuestra = new JPanel();
         panelFiguraMuestra.add(lblFiguraMuestra);
 
-        JPanel panelFiguras = new JPanel(new GridLayout(1, 3)); // Cambiamos a 1 fila y 3 columnas
+        JPanel panelFiguras = new JPanel(new GridLayout(1, 3));
         for (JLabel label : figurasLabels) {
             panelFiguras.add(label);
         }
 
-        JPanel panelDatos = new JPanel();
-        panelDatos.setLayout(new BoxLayout(panelDatos, BoxLayout.Y_AXIS));
+        JPanel panelDatos = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
         panelDatos.add(lblNombre);
         panelDatos.add(lblNivel);
         panelDatos.add(lblIntentos);
@@ -104,10 +102,13 @@ public class VentanaJuego extends JFrame {
         JPanel panelBotonTerminar = new JPanel();
         panelBotonTerminar.add(btnTerminarJuego);
 
-        add(panelFiguraMuestra, BorderLayout.NORTH); // Cambiamos la ubicación de la imagen de muestra
+        JPanel panelInferior = new JPanel(new BorderLayout());
+        panelInferior.add(panelDatos, BorderLayout.NORTH);
+        panelInferior.add(panelBotonTerminar, BorderLayout.SOUTH);
+
+        add(panelFiguraMuestra, BorderLayout.NORTH);
         add(panelFiguras, BorderLayout.CENTER);
-        add(panelDatos, BorderLayout.EAST);
-        add(panelBotonTerminar, BorderLayout.SOUTH);
+        add(panelInferior, BorderLayout.SOUTH);
     }
 
     private void setupActions() {
@@ -137,11 +138,53 @@ public class VentanaJuego extends JFrame {
         if (rutaSeleccionada.equals(lblFiguraMuestra.getName())) {
             // La imagen seleccionada es la correcta
             JOptionPane.showMessageDialog(null, "¡Felicitaciones! Has seleccionado la opción correcta.");
-            // Aquí puedes implementar la lógica para avanzar al siguiente nivel
+            nivel++;
+            actualizarDatosJuego();
         } else {
             // La imagen seleccionada es incorrecta
             JOptionPane.showMessageDialog(null, "Has seleccionado la opción incorrecta.");
-            // Aquí puedes implementar la lógica para aumentar el contador de fallos
+            fallos++;
+            actualizarDatosJuego();
+        }
+    }
+
+    private void actualizarDatosJuego() {
+        lblNivel.setText("Nivel: " + nivel);
+        intentos++;
+        lblIntentos.setText("Intentos: " + intentos);
+        lblFallos.setText("Fallos: " + fallos);
+
+        List<String> rutas = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            rutas.add("src/imagenes/nivel" + nivel + "/" + i + ".png");
+        }
+
+        // Shuffle para que las imágenes estén en orden aleatorio
+        Collections.shuffle(rutas);
+
+        // Elegimos una imagen aleatoria que se mostrará dos veces (como muestra y opción)
+        Random random = new Random();
+        int indiceImagenDuplicada = random.nextInt(rutas.size());
+        String rutaImagenDuplicada = rutas.get(indiceImagenDuplicada);
+
+        // Las siguientes imágenes serán las opciones (las primeras 3)
+        for (int i = 0; i < figurasLabels.length; i++) {
+            String rutaOpcion;
+            if (i == 3) {
+                // Si es la última opción, usamos la ruta duplicada como imagen de muestra
+                rutaOpcion = rutaImagenDuplicada;
+            } else {
+                rutaOpcion = rutas.get(i);
+            }
+
+            ImageIcon iconoOpcion = new ImageIcon(rutaOpcion);
+            figurasLabels[i].setIcon(iconoOpcion);
+            figurasLabels[i].setName(rutaOpcion);
+
+            if (i == 3) {
+                lblFiguraMuestra.setIcon(iconoOpcion);
+                lblFiguraMuestra.setName(rutaOpcion);
+            }
         }
     }
 
