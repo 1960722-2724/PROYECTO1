70845e3affabcb82a26bca341f297006ab3ccbe1
1960722-2724 <146265513+1960722-2724@users.noreply.github.com>
2023-10-08 17:37:24 -0500

    Update Header.java

--- juego figura/src/vista/Header.java
+++ juego figura/src/vista/Header.java
@@ -1,1 +1,26 @@
+package vista;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Header extends JPanel {
+    private ImageIcon imagen;
+    private String nombre;
+
+    public Header(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Dimension tamaño = getSize();
+        imagen = new ImageIcon(getClass().getResource(nombre));
+        g.drawImage(imagen.getImage(), 0, 0, null);
+        imagen.getImage().flush(); // Libera los recursos de la imagen
+        setOpaque(false);
+        super.paint(g);
+    }
+}
 
